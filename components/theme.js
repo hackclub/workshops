c369f0c1af48fc7cbbef42f707a518361d2578ac
Lachlan Campbell <lachlan@hackclub.com>
2020-01-12 21:34:53 -0500

    Streamline theme

--- components/theme.js
+++ components/theme.js
@@ -1,12 +1,16 @@
-import { base } from '@theme-ui/presets'
-import { merge } from 'lodash'
-
 export const breakpoints = [32, 48, 64, 96, 128].map(w => `${w}em`)
+
+const heading = {
+  color: 'text',
+  lineHeight: 'heading',
+  fontWeight: 'bold'
+}
 
 export const palette = {
   darker: '#121217',
   dark: '#17171d',
   darkless: '#252429',
+
   black: '#1f2d3d',
   steel: '#273444',
   slate: '#3c4858',
@@ -14,18 +18,20 @@ export const palette = {
   smoke: '#e0e6ed',
   snow: '#f9fafc',
   white: '#ffffff',
+
   red: '#ec3750',
   orange: '#ff8c37',
   yellow: '#f1c40f',
   green: '#33d6a6',
   cyan: '#5bc0de',
   blue: '#338eda',
+
   twitter: '#1da1f2',
   facebook: '#3b5998',
   instagram: '#e1306c'
 }
 
-const theme = merge(base, {
+export const theme = {
   breakpoints,
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   fontSizes: [12, 16, 20, 24, 32, 48, 64, 96, 128],
@@ -43,10 +49,6 @@ const theme = merge(base, {
     primary: palette.red,
     muted: palette.muted,
     accent: palette.blue,
-    invertedPrimary: palette.white,
-    invertedText: palette.white,
-    header: palette.snow,
-    nav: palette.white,
     modes: {
       dark: {
         text: palette.white,
@@ -57,11 +59,7 @@ const theme = merge(base, {
         placeholder: palette.slate,
         secondary: palette.muted,
         muted: palette.muted,
-        accent: palette.cyan,
-        invertedPrimary: palette.darker,
-        invertedText: palette.dark,
-        header: palette.darkless,
-        nav: palette.red
+        accent: palette.cyan
       }
     }
   },
@@ -85,11 +83,13 @@ const theme = merge(base, {
     headline: '0.009em'
   },
   sizes: {
-    ultrawide: 2048,
+    widePlus: 2048,
     wide: 1536,
-    container: 1200,
-    subcontainer: 1024,
-    narrowplus: 768,
+    layoutPlus: 1200,
+    layout: 1024,
+    copyPlus: 768,
+    copy: 680,
+    narrowPlus: 600,
     narrow: 512
   },
   radii: {
@@ -158,7 +158,7 @@ const theme = merge(base, {
       bg: 'primary',
       color: 'background',
       cursor: 'pointer',
-      fontFamily: 'body',
+      fontFamily: 'inherit',
       fontWeight: 'bold',
       svg: { ml: -1, mr: 2 }
     },
@@ -166,7 +166,7 @@ const theme = merge(base, {
       bg: 'invertedPrimary',
       color: 'invertedText',
       cursor: 'pointer',
-      fontFamily: 'body',
+      fontFamily: 'inherit',
       fontWeight: 'bold',
       svg: { ml: -1, mr: 2 }
     }
@@ -175,7 +175,7 @@ const theme = merge(base, {
     input: {
       bg: 'elevated',
       color: 'text',
-      fontFamily: 'body',
+      fontFamily: 'inherit',
       borderRadius: 'base',
       boxShadow: 'small',
       transition: 'box-shadow .125s ease-in-out',
@@ -218,30 +218,96 @@ const theme = merge(base, {
   layout: {
     container: {
       width: '100%',
-      maxWidth: ['container', null, null, null, 'wide'],
+      maxWidth: ['layout', null, 'layoutPlus', null, 'wide'],
       mx: 'auto',
       px: 3
     },
     wide: {
       width: '100%',
-      maxWidth: ['wide', null, null, null, 'ultrawide'],
+      maxWidth: ['wide', null, null, null, 'widePlus'],
+      mx: 'auto',
+      px: 3
+    },
+    copy: {
+      width: '100%',
+      maxWidth: ['copy', null, null, null, 'copyPlus'],
       mx: 'auto',
       px: 3
     },
     narrow: {
       width: '100%',
-      maxWidth: ['narrow', null, null, null, 'container'],
+      maxWidth: ['narrow', null, 'narrowPlus', null, 'layout'],
       mx: 'auto',
       px: 3
     }
   },
   styles: {
+    h1: {
+      ...heading,
+      fontSize: 5
+    },
+    h2: {
+      ...heading,
+      fontSize: 4
+    },
+    h3: {
+      ...heading,
+      fontSize: 3
+    },
+    h4: {
+      ...heading,
+      fontSize: 2
+    },
+    h5: {
+      ...heading,
+      fontSize: 1
+    },
+    h6: {
+      ...heading,
+      fontSize: 0
+    },
+    p: {
+      color: 'text',
+      fontFamily: 'inherit',
+      fontWeight: 'body',
+      lineHeight: 'body'
+    },
+    a: {
+      color: 'primary'
+    },
+    pre: {
+      fontFamily: 'monospace',
+      overflowX: 'auto',
+      code: {
+        color: 'inherit'
+      }
+    },
+    code: {
+      fontFamily: 'monospace',
+      fontSize: 'inherit'
+    },
+    table: {
+      width: '100%',
+      borderCollapse: 'separate',
+      borderSpacing: 0
+    },
+    th: {
+      textAlign: 'left',
+      borderBottomStyle: 'solid'
+    },
+    td: {
+      textAlign: 'left',
+      borderBottomStyle: 'solid'
+    },
+    img: {
+      maxWidth: '100%'
+    },
     hr: {
       borderColor: 'border',
       my: [3, 4]
     }
   }
-})
+}
 theme.styles.root = {
   fontFamily: theme.fonts.body,
   lineHeight: theme.lineHeights.body,
