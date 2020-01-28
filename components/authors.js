291c969ac8cc1f87291379d30253607e123a5c0d
Lachlan Campbell <lachlan@hackclub.com>
2020-01-27 22:18:53 -0500

    Support non-username authors

--- components/authors.js
+++ components/authors.js
@@ -6,27 +6,33 @@ export default ({ text = '@lachlanjc', sx = {}, ...props }) => (
     {...props}
     sx={{ justifyContent: 'center', alignItems: 'center', ...sx }}
   >
-    {text
-      .split(',')
-      .map(trim)
-      .map(name => (
-        <Flex
-          key={name}
-          as="a"
-          href={`https://github.com/${name.replace('@', '')}`}
-          title={`View ${name} on GitHub`}
-          sx={{ alignItems: 'center', m: [1, 2] }}
-        >
-          <Avatar
-            src={`https://github.com/${name.replace('@', '')}.png`}
-            size={[32, 48, 64]}
-            alt={name.replace('@', '')}
-            sx={{ mr: 2 }}
-          />
-          <Text as="span" color="muted" sx={{ fontSize: [1, 2] }}>
-            {name}
-          </Text>
-        </Flex>
-      ))}
+    {text.includes('@') ? (
+      text
+        .split(',')
+        .map(trim)
+        .map(name => (
+          <Flex
+            key={name}
+            as="a"
+            href={`https://github.com/${name.replace('@', '')}`}
+            title={`View ${name} on GitHub`}
+            sx={{ alignItems: 'center', m: [1, 2] }}
+          >
+            <Avatar
+              src={`https://github.com/${name.replace('@', '')}.png`}
+              size={[32, 48, 64]}
+              alt={name.replace('@', '')}
+              sx={{ mr: 2 }}
+            />
+            <Text as="span" color="muted" sx={{ fontSize: [1, 2] }}>
+              {name}
+            </Text>
+          </Flex>
+        ))
+    ) : (
+      <Text as="span" color="muted" sx={{ fontSize: [1, 2] }}>
+        {text}
+      </Text>
+    )}
   </Flex>
 )
