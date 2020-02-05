88ccc670651658fc9905ad23f64f1b00a6ac19cd
Lachlan Campbell <lachlan@hackclub.com>
2020-02-05 01:41:21 -0500

    Reduce homepage code

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,38 @@
 import dynamic from 'next/dynamic'
-import theme from '@hackclub/theme'
-import { Button, useColorMode } from 'theme-ui'
+import { Box, Button, useColorMode } from 'theme-ui'
 import Header from '../components/header'
 import Listing from '../components/listing'
-import styled from '@emotion/styled'
+
 const Blobs = dynamic(import('../components/blobs'), { ssr: false })
 
-export default ({ sections }) => {
-  const [mode] = useColorMode()
-
-  const Wrapper = styled.div`
-    z-index: -100;
-    position: fixed;
-    background-color: ${mode === 'dark' ? theme.colors.darkless : theme.colors.sheet}
-`
-
-  return (
-    <>
-      <Wrapper>
-        <Blobs />
-      </Wrapper>
-
-      <Header
-        title="Hack Club Workshops"
-        desc="Learn to code with this collection of community-contributed, self-guided coding tutorials + ideas."
-        sx={{ mb: 0 }}
+export default ({ sections }) => (
+  <>
+    <Box sx={{ zIndex: -1, position: 'fixed', bg: 'sheet' }}>
+      <Blobs />
+    </Box>
+    <Header
+      title="Hack Club Workshops"
+      desc="Learn to code with this collection of community-contributed, self-guided coding tutorials + ideas."
+      sx={{ mb: 0, bg: 'transparent' }}
+    >
+      <Button
+        as="a"
+        href="https://hackclub.com/philosophy/"
+        variant="outline"
+        sx={{
+          mt: [3, 4],
+          transition: '.125s transform ease-in-out',
+          ':hover,:focus': { transform: 'scale(1.0625)' }
+        }}
       >
-        <Button
-          as="a"
-          href="https://hackclub.com/philosophy/"
-          variant="outline"
-          sx={{
-            mt: [3, 4],
-            transition: '.125s transform ease-in-out',
-            ':hover,:focus': { transform: 'scale(1.0625)' }
-          }}
-        >
-          Our Philosophy »
-        </Button>
-      </Header>
-
-      {sections.map(({ key, ...section }) => (
-        <Listing key={key} id={key} {...section} />
-      ))}
-    </>
-  )
-}
+        Our Philosophy »
+      </Button>
+    </Header>
+    {sections.map(({ key, ...section }) => (
+      <Listing key={key} id={key} {...section} />
+    ))}
+  </>
+)
 
 export const unstable_getStaticProps = async () => {
   const { getWorkshopSections } = require('../lib/data')
