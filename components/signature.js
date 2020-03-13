731fb12d52b35bb5eee108de2c33a0f00c5320be
Lachlan Campbell <lachlan@hackclub.com>
2020-03-13 00:12:25 -0400

    Make images same-origin

--- components/signature.js
+++ components/signature.js
@@ -4,11 +4,9 @@ const Signature = () => {
   const [colorMode] = useColorMode()
   return (
     <Image
-      src={
-        colorMode === 'dark'
-          ? 'https://lh6.googleusercontent.com/zN4HoAfUfEqjJpCwkxutNWpyxUQaeDAbIJ4gNfC69N_aP7AYo8szr7yYZKIQpVkvEmCy5LG24GxWWW3p1TaBvlm8FcEwmCcYq9oV_izt0hp8elRyQhbT7bSLc3MKKJMVkMZj9kZq'
-          : 'https://lh4.googleusercontent.com/cWSIcZNYFMWe1_Ka3Yt4pIuvrj3fZElBrZbuOLSl6wZYaLUTE1OsY_wq_h5Ta4nT4RxLag1er_w5W-Vi99Yu0qBNUZZ0JtobuRPNgJZecro-qL61sYkYhnVdIVAyIC5ISeszyo-h'
-      }
+      src={`https://workshops.hackclub.com/signature-${
+        colorMode === 'dark' ? 'light' : 'dark'
+      }.png`}
       width={96}
       alt="Zach's signature"
     />
