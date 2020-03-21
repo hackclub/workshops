6ae9abc64f302a60d37d200162a11e7d4f3d2121
Lachlan Campbell <lachlan@hackclub.com>
2020-03-21 17:36:45 -0400

    Add assetPrefix for fixing hackclub.com rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
 const withMDX = require('@zeit/next-mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
+  assetPrefix: isProd ? 'https://workshops.hackclub.com' : '',
   webpack: config => {
     config.node = { fs: 'empty' }
     return config
