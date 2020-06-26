b98d446591fa530ce447f04aca6d8098450e31f0
Lachlan Campbell <lachlan@hackclub.com>
2020-06-26 18:57:46 +0000

    Add Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,35 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import NextApp from 'next/app'
+import Head from 'next/head'
 
+import Meta from '@hackclub/meta'
 import '@hackclub/theme/fonts/reg-ital-bold.css'
-import { ThemeProvider } from 'theme-ui'
 import theme from '../lib/theme'
 import NProgress from '../components/nprogress'
+import { ThemeProvider } from 'theme-ui'
+import * as Fathom from 'fathom-client'
 
-export default class App extends NextApp {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider theme={theme}>
-        <NProgress color={theme.colors.primary} />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    )
-  }
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load('UDLVKCLR', {
+      includedDomains: ['hackclub.com', 'workshops.hackclub.com']
+    })
+    const onRouteChangeComplete = () => Fathom.trackPageview()
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <NProgress color={theme.colors.primary} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+export default App
