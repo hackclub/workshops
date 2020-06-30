e4fc7a058135f169d1dffa794a24a680858404f6
Lachlan Campbell <lachlan@hackclub.com>
2020-06-30 18:08:23 +0000

    Upgrade Vercelness

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production'
-const withMDX = require('@zeit/next-mdx')({ extension: /\.mdx?$/ })
+const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
+console.log('VERCEL URL', process.env.VERCEL_URL)
 module.exports = withMDX({
+  experimental: { trailingSlash: true },
   pageExtensions: ['js', 'jsx', 'mdx'],
   assetPrefix: isProd ? 'https://workshops.hackclub.com' : '',
   webpack: config => {
