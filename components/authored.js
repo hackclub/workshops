6ebda93e9c3b7d9ea7ba2a6b1d9546e29e8df3b1
Lachlan Campbell <lachlan@hackclub.com>
2020-08-21 13:58:27 -0400

    Merge pull request #20 from hackclub/hc-134-letterhead
    Standardize Letterhead for open letters

---
+++ components/authored.js
@@ -0,0 +1,57 @@
+import { Avatar, Badge, Flex } from 'theme-ui'
+
+const Authored = ({ name, avatar, url, date, ...props }) => (
+  <Flex
+    mt={3}
+    {...props}
+    sx={{
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      alignItems: 'center',
+      'a, span, time': {
+        mt: 0,
+        mb: 2,
+        bg: 'white',
+        color: 'muted',
+        border: '1px solid',
+        borderColor: 'background',
+        bg: 'sunken',
+        fontSize: 2,
+        fontWeight: 'body',
+        lineHeight: '36px',
+        '@media print': {
+          bg: 'transparent',
+          border: 0,
+          fontSize: 1
+        }
+      }
+    }}
+  >
+    <Badge
+      variant="pill"
+      as={url ? 'a' : 'span'}
+      href={url}
+      sx={{
+        mr: [2, 3],
+        pl: 1,
+        pr: 3,
+        display: 'inline-flex',
+        alignItems: 'center',
+        textDecoration: 'none'
+      }}
+    >
+      <Avatar src={avatar} alt={name} size={36} mr={2} />
+      {name}
+    </Badge>
+    <Badge
+      variant="pill"
+      as="time"
+      px={3}
+      dateTime={date?.startsWith('20') ? date : undefined}
+    >
+      {date}
+    </Badge>
+  </Flex>
+)
+
+export default Authored
---
+++ components/letterhead.js
@@ -0,0 +1,60 @@
+import Head from 'next/head'
+import Meta from '@hackclub/meta'
+import Header from '../components/header'
+import Authored from '../components/authored'
+import { Container, Button } from 'theme-ui'
+import { GitHub } from 'react-feather'
+import { Styled as Content } from '../components/content'
+
+const Letterhead = ({
+  title,
+  desc,
+  author = { name: null, avatar: null, url: null },
+  date,
+  img,
+  path,
+  includeMeta = true,
+  hideGitHub = false,
+  children,
+  ...props
+}) => (
+  <>
+    {includeMeta && (
+      <Meta
+        as={Head}
+        title="COVID-19"
+        description={
+          author?.name && date
+            ? `${desc} Written by ${author.name} on ${date}.`
+            : desc
+        }
+        image={img}
+      />
+    )}
+    <Header title={title} desc={desc} {...props}>
+      {author?.name && <Authored {...author} date={date} />}
+    </Header>
+    <Container as={Content} variant="copy" pt={3} pb={[4, 5]}>
+      {children}
+      {!hideGitHub && (
+        <Button
+          as="a"
+          target="_blank"
+          href={`https://github.com/hackclub/workshops/blob/main/pages/${path}`}
+          variant="outline"
+          sx={{
+            mt: 2,
+            fontSize: 1,
+            textDecoration: 'none !important',
+            '@media print': { display: 'none' }
+          }}
+        >
+          <GitHub />
+          Edit on GitHub
+        </Button>
+      )}
+    </Container>
+  </>
+)
+
+export default Letterhead
