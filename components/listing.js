c81fc89b77e16b02acdc8b128c19265bd4334ac0
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-09-18 23:22:19 +0800

    Add Workshops Bounties section + me! (#31)
    * Update manifest.json
    
    * Update manifest.json
    
    * Needed to add the image Zach asked for :D

--- components/listing.js
+++ components/listing.js
@@ -21,7 +21,7 @@ const WorkshopCard = ({ slug, name, description, section }) => (
         </Heading>
         <Text variant="caption">{description}</Text>
       </Box>
-      {(section === 'starters' || section === 'web') && (
+      {(section === 'starters' || section === 'web' || section === 'bounties' ) && (
         <Image
           alt="Demo"
           src={`/content/workshops/${slug}/img/demo.png`}
