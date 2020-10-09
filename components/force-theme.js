61e813a0e59295973d85667fcc59637d059747f0
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-09 09:25:58 +0800

    Update force-theme.js

--- components/force-theme.js
+++ components/force-theme.js
@@ -1,7 +1,14 @@
 import { useColorMode } from 'theme-ui'
 
 export default ({ theme }) => {
-  const [colorMode, setColorMode] = useColorMode()
-  setColorMode(theme)
-  return null
+ const [colorMode, setColorMode] = useColorMode()
+ setColorMode(theme)
+ return null
+ return (
+   <style jsx global>{`
+     .nav-color-switcher{
+       display: none;
+     }`}
+   </style>
+   )
 }
