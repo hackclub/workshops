117e7f13204ca4b20896075710abbae8d2bcbc6b
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-01-05 20:20:12 +0800

    Add External Previews for Workshops (#52)
    * external previews :tada:
    
    * Fixed!

---
+++ pages/external-preview/[repo]/[branch]/[slug].js
@@ -0,0 +1,37 @@
+import { Container } from 'theme-ui'
+import Error from 'next/error'
+import Header from '../../../../components/header'
+import Authors from '../../../../components/authors'
+import Content from '../../../../components/content'
+
+const Page = ({ slug, data, html }) => {
+  if (!slug || !data) return <Error statusCode={404} />
+  return (
+    <>
+      <Header
+        title={data.name}
+        desc={data.description}
+        img={data.card}
+        bgImg={data.bg}
+        includeMeta
+      >
+        <Authors text={data.author} />
+      </Header>
+      <Container variant="copy" as="main">
+        <Content html={html} />
+      </Container>
+    </>
+  )
+}
+
+Page.getInitialProps = async req => {
+  const { getRawFileFromRepo } = require('../../../../lib/github')
+  const { getWorkshopData } = require('../../../../lib/data')
+  let { branch, repo, slug } = req.query
+  repo = repo + '/hackclub'
+  const md = await getRawFileFromRepo(`workshops/${slug}/README.md`, branch, repo)
+  const { data, html } = await getWorkshopData(slug, md, branch)
+  return { slug, data, html }
+}
+
+export default Page
