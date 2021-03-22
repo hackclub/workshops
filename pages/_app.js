0e9efde121f8e8bf2288d3de4cce60b836c63390
Max Wofford <max@maxwofford.com>
2021-03-22 15:19:58 -0400

    Remove Fathom analytics (#59)

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,15 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import NextApp from 'next/app'
-import Head from 'next/head'
+import React from 'react'
 
-import Meta from '@hackclub/meta'
 import '@hackclub/theme/fonts/reg-ital-bold.css'
 import theme from '../lib/theme'
 import NProgress from '../components/nprogress'
 import { ThemeProvider } from 'theme-ui'
-import * as Fathom from 'fathom-client'
 
-const App = ({ Component, pageProps }) => {
-  const router = useRouter()
-
-  useEffect(() => {
-    Fathom.load('UDLVKCLR', {
-      includedDomains: ['hackclub.com', 'workshops.hackclub.com']
-    })
-    const onRouteChangeComplete = () => Fathom.trackPageview()
-    router.events.on('routeChangeComplete', onRouteChangeComplete)
-    return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
-
-  return (
-    <ThemeProvider theme={theme}>
-      <NProgress color={theme.colors.primary} />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
-}
+const App = ({ Component, pageProps }) => (
+  <ThemeProvider theme={theme}>
+    <NProgress color={theme.colors.primary} />
+    <Component {...pageProps} />
+  </ThemeProvider>
+)
 
 export default App
