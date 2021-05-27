f20c6effef70df22ef68524b8db38c44f765e1c2
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-05-27 19:10:57 +0800

    Update workshops.js

--- pages/api/workshops.js
+++ pages/api/workshops.js
@@ -1,4 +1,26 @@
 export default async (req, res) => {
+  const fs = require('fs')
+  const download = require('download-git-repo')
+
+  const repos = {
+    'hackclub/hackclub#main': './public/content',
+    'hackclub/vip-newsletters#main': './public/vip-newsletters'
+  }
+
+  const downloadRepo = async (repo, path) =>
+    await download(repo, path, err =>
+      err ? console.error(err) : console.log(`Successfully downloaded ${repo}!`)
+    )
+
+  const getRepo = async (repo, path) => {
+    try {
+      return fs.readdirSync(`${path}/`).length > 1
+    } catch {
+      await downloadRepo(repo, path)
+    }
+  }
+
+  Object.keys(repos).map(key => getRepo(key, repos[key]))
   const { getWorkshopSections } = require('../../lib/data')
   const sections = await getWorkshopSections()
   res.json(sections)
