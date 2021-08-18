eb19a869241a19292f4ec8b8d2fefa15811fffdf
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-18 19:54:44 +0800

    Update workshops.js

--- pages/api/workshops.js
+++ pages/api/workshops.js
@@ -1,5 +1,21 @@
 import {getWorkshopSections} from '../../lib/data'
 
-export default async (req, res) => {
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another option
+  // res.setHeader('Access-Control-Allow-Origin', req.header.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res)
+}
+
+async function handler(req, res) => {
     res.json((await getWorkshopSections()))
 }
+
+module.exports = allowCors(handler)
