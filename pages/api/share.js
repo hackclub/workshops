4dbeec5ef57b5ebaf721d75b0f37f8dced246d65
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-10-20 21:54:48 +0800

    Add a share button!

---
+++ pages/api/share.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res){
+  let submission = await fetch(
+    'https://airtable-forms-proxy.hackclub.dev/api/appYdkBrT3PrwcbJB/Workshops/',
+    {
+      method: 'POST',
+      body: JSON.stringify(req.body),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+  submission = await submission.json()
+  console.log(submission)
+  res.json(submission)
+}
