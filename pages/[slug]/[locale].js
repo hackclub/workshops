450f3db509bac6ac4e454db5b7eaa34c4aaea653
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-12-05 13:28:45 +0800

    Push for Testing

---
+++ pages/[slug]/[locale].js
@@ -0,0 +1,57 @@
+import { map } from 'lodash'
+import { Button, Container, Flex, Spinner } from 'theme-ui'
+import Error from 'next/error'
+import Header from '../../components/header'
+import Authors from '../../components/authors'
+import Content from '../../components/content'
+import Footer from '../../components/footer'
+import Share from '../../components/share'
+import { useRouter } from 'next/router'
+
+const Page = ({ slug, data, html }) => {
+  const router = useRouter()
+  if (router.isFallback) {
+    return (
+      <Flex
+        sx={{
+          minHeight: '100vh',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Spinner />
+      </Flex>
+    )
+  }
+  if (!slug || !data) return <Error statusCode={404} />
+  return (
+    <>
+      <Header
+        title={data.name}
+        desc={data.description}
+        img={data.card}
+        bgImg={data.bg}
+        includeMeta
+      >
+        <Authors text={data.author} />
+      </Header>
+      <Container variant="copy" as="main">
+        {data.locales}
+        <Content html={html} />
+        <Share workshop={data.name} />
+      </Container>
+      <Footer />
+    </>
+  )
+}
+
+export const getServerSideProps = async ({ params }) => {
+  const { getWorkshopFile, getWorkshopData } = require('../../lib/data')
+  const { slug, locale } = params
+  const md = await getWorkshopFile(slug, locale)
+  const { data, html } = await getWorkshopData(slug, md)
+  console.log(data)
+  return { props: { slug, data, html } }
+}
+
+export default Page
--- pages/[slug]/index.js
+++ pages/[slug]/index.js
@@ -1,11 +1,11 @@
 import { map } from 'lodash'
 import { Button, Container, Flex, Spinner } from 'theme-ui'
 import Error from 'next/error'
-import Header from '../components/header'
-import Authors from '../components/authors'
-import Content from '../components/content'
-import Footer from '../components/footer'
-import Share from '../components/share'
+import Header from '../../components/header'
+import Authors from '../../components/authors'
+import Content from '../../components/content'
+import Footer from '../../components/footer'
+import Share from '../../components/share'
 import { useRouter } from 'next/router'
 
 const Page = ({ slug, data, html }) => {
@@ -45,17 +45,18 @@ const Page = ({ slug, data, html }) => {
 }
 
 export const getStaticPaths = async () => {
-  const { getWorkshopSlugs } = require('../lib/data')
+  const { getWorkshopSlugs } = require('../../lib/data')
   const slugs = await getWorkshopSlugs()
   const paths = map(slugs, slug => ({ params: { slug } }))
   return { paths, fallback: true }
 }
 
 export const getStaticProps = async ({ params }) => {
-  const { getWorkshopFile, getWorkshopData } = require('../lib/data')
+  const { getWorkshopFile, getWorkshopData } = require('../../lib/data')
   const { slug } = params
   const md = await getWorkshopFile(slug)
   const { data, html } = await getWorkshopData(slug, md)
+  console.log(data)
   return { props: { slug, data, html }, revalidate: 30 }
 }
 
