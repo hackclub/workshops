07c152a6eb8d09eca6d717fe80cd2c5dce348f32
Belle <65808924+bellesea@users.noreply.github.com>
2022-05-29 09:38:37 +0800

    add copy to section links (#104)
    * add copy to section links
    
    * update tooltip color
    
    * change icon size & color
    
    * fix width of tooltip

--- components/listing.js
+++ components/listing.js
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import { Box, Container, Heading, Text, Grid, Card, Image } from 'theme-ui'
 import Link from 'next/link'
 import NextImage from 'next/image'
 import { snakeCase } from 'lodash'
+import Icon from '@hackclub/icons'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 const WorkshopCard = ({ slug, name, description, img, section }) => (
   <Link href={`/${slug}`} passHref>
@@ -50,41 +53,93 @@ const WorkshopCard = ({ slug, name, description, img, section }) => (
   </Link>
 )
 
-const Listing = ({ id, title, description, workshops, ...props }) => (
-  <Box
-    as="section"
-    id={snakeCase(title)}
-    sx={{
-      backgroundImage: theme =>
-         `linear-gradient(to bottom, ${theme.colors.sheet}, ${theme.colors.sunken})`,
-      py: [4, 5],
-      flex: workshops.length == 0 ? 1 : null, //fix for floating footer on fallback data being rendered
-      color: 'text',
-      '@media print': { background: 'none !important' }
-    }}
-    {...props}
-  >
-    <Container>
-      <Heading as="h2" variant="headline" sx={{ mt: 0, mb: 1 }}>
-        {title}
-      </Heading>
-      <Text variant="caption" sx={{ fontSize: 2 }}>
-        {description}
-      </Text>
-      <Grid
-        gap={[3, 4]}
-        columns={[null, 2, 4]}
-        sx={{
-          mt: [3, 4],
-          '@media print': { gridTemplateColumns: 'repeat(2,1fr)' }
-        }}
-      >
-        {workshops.map(workshop => (
-          <WorkshopCard key={workshop.slug} section={id} {...workshop} />
-        ))}
-      </Grid>
-    </Container>
-  </Box>
-)
-
+function Listing({ id, title, description, workshops, ...props }) {
+  const [copied, setCopied] = useState(false)
+  return (
+    <Box
+      as="section"
+      id={snakeCase(title)}
+      sx={{
+        backgroundImage: `theme.util.gx('sheet', 'sunken')`,
+        py: [4, 5],
+        flex: workshops.length == 0 ? 1 : null, //fix for floating footer on fallback data being rendered
+        color: 'text',
+        '@media print': { background: 'none !important' }
+      }}
+      {...props}
+    >
+      <Container>
+        <CopyToClipboard
+          text={`https://workshops.hackclub.com#${snakeCase(title)}`}
+          onCopy={() => {
+            setCopied(true)
+            setTimeout(() => {
+              setCopied(false)
+            }, 3000)
+          }}
+        >
+          <Box sx={{ display: 'table-cell', verticalAlign: 'middle' }}>
+            <Heading
+              as="h2"
+              variant="headline"
+              sx={{ mt: 0, mb: 1, display: 'inline', width: 'fit-content' }}
+            >
+              {title}
+            </Heading>
+            <Box
+              sx={{
+                position: 'relative',
+                display: 'inline',
+                width: 'fit-content',
+                color: 'muted',
+                pl: '5px',
+                ':hover,:focus': {
+                  cursor: 'pointer',
+                  '> span': {
+                    display: 'block'
+                  }
+                }
+              }}
+            >
+              <Icon glyph="link" size={24} />
+              <Text
+                sx={{
+                  position: 'absolute',
+                  right: '-350%',
+                  bottom: '0',
+                  display: 'none',
+                  backgroundColor: '#1f2d3d',
+                  color: 'white',
+                  opacity: '80%',
+                  px: '5px',
+                  py: '2px',
+                  borderRadius: '5px',
+                  width: '320%',
+                  textAlign: 'center'
+                }}
+              >
+                {copied ? 'Copied!' : 'Copy Link'}
+              </Text>
+            </Box>
+          </Box>
+        </CopyToClipboard>
+        <Text variant="caption" sx={{ fontSize: 2 }}>
+          {description}
+        </Text>
+        <Grid
+          gap={[3, 4]}
+          columns={[null, 2, 4]}
+          sx={{
+            mt: [3, 4],
+            '@media print': { gridTemplateColumns: 'repeat(2,1fr)' }
+          }}
+        >
+          {workshops.map(workshop => (
+            <WorkshopCard key={workshop.slug} section={id} {...workshop} />
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  )
+}
 export default Listing
