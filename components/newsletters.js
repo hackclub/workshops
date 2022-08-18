680531cccfc92f3ef1eec444c98e0d22c82bc5c0
warren yun <wyun2006@gmail.com>
2022-08-18 00:37:50 -0400

    separate styles between vip and community

--- components/newsletters.js
+++ components/newsletters.js
@@ -7,35 +7,66 @@ const getColor = i => colors[Number(i - 1) % colors.length]
 
 export default ({ issues, showAbout, vip = true }) => {
   const { pathname, query } = useRouter()
-  const active = pathname.startsWith('/vip-newsletters/') || pathname.startsWith('/newsletters/') ? query.slug : false
+  const active =
+    pathname.startsWith('/vip-newsletters/') ||
+    pathname.startsWith('/newsletters/')
+      ? query.slug
+      : false
   const { theme } = useThemeUI()
   return (
     <>
-      <Grid columns={[2, 3, 4]} gap={3} sx={{ alignItems: 'center' }}>
-        {issues.map(issue => (
-          <Link
-            href={`/${vip ? 'vip-' : ''}newsletters/[slug]`}
-            as={`/${vip ? 'vip-' : ''}newsletters/${issue}`}
-            passHref
-            key={issue}
-          >
-            <Card
-              as="a"
-              variant="nav"
-              sx={{ bg: getColor(issue), color: 'white' }}
-              style={{
-                boxShadow:
-                  active === issue
-                    ? `0 0 0 3px ${theme.colors.sheet}, 0 0 0 6px ${
-                        theme.colors[getColor(issue)]
-                      }`
-                    : theme.shadows.card
-              }}
-            >
-              {issue}
-            </Card>
-          </Link>
-        ))}
+      <Grid columns={vip ? [2, 3, 4] : [2, 3]} gap={3} sx={{ alignItems: 'center' }}>
+        {vip
+          // VIP newsletter styles
+          ? issues.map(issue => (
+              <Link
+                href={`/vip-newsletters/[slug]`}
+                as={`/vip-newsletters/${issue}`}
+                passHref
+                key={issue}
+              >
+                <Card
+                  as="a"
+                  variant="nav"
+                  sx={{ bg: getColor(issue), color: 'white' }}
+                  style={{
+                    boxShadow:
+                      active === issue
+                        ? `0 0 0 3px ${theme.colors.sheet}, 0 0 0 6px ${
+                            theme.colors[getColor(issue)]
+                          }`
+                        : theme.shadows.card
+                  }}
+                >
+                  {issue}
+                </Card>
+              </Link>
+            ))
+          // Community news letter issues
+          : issues.map(issue => (
+              <Link
+                href={`/newsletters/[slug]`}
+                as={`/newsletters/${issue}`}
+                passHref
+                key={issue}
+              >
+                <Card
+                  as="a"
+                  variant="nav"
+                  sx={{ bg: getColor(issue), color: 'white' }}
+                  style={{
+                    boxShadow:
+                      active === issue
+                        ? `0 0 0 3px ${theme.colors.sheet}, 0 0 0 6px ${
+                            theme.colors[getColor(issue)]
+                          }`
+                        : theme.shadows.card
+                  }}
+                >
+                  {issue}
+                </Card>
+              </Link>
+            ))}
       </Grid>
     </>
   )
