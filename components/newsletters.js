72d4b4b0bf4ca32edbba2e664a4be8f8b84b0560
Echo <github@3kh0.net>
2026-01-25 09:46:34 -0500

    fix ssr errors

--- components/newsletters.js
+++ components/newsletters.js
@@ -1,20 +1,26 @@
-import { Grid, Card, useThemeUI } from 'theme-ui'
+import { Grid, Card } from 'theme-ui'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 import { formatTitle } from '../lib/format-title'
 
 const colors = 'red,orange,yellow,green,cyan,blue,purple'.split(',')
 const getColor = i => colors[Number(i - 1) % colors.length]
 
 export default ({ issues, showAbout, kind = '' }) => {
-  const { pathname, query } = useRouter()
-  const active =
-    pathname.startsWith('/leader-newsletters/') ||
-    pathname.startsWith('/vip-newsletters/') ||
-    pathname.startsWith('/newsletter/')
-      ? query.slug
-      : false
-  const { theme } = useThemeUI()
+  const { pathname, query, isReady } = useRouter()
+  const [active, setActive] = useState(false)
+  
+  useEffect(() => {
+    if (isReady) {
+      const isActive =
+        pathname.startsWith('/leader-newsletters/') ||
+        pathname.startsWith('/vip-newsletters/') ||
+        pathname.startsWith('/newsletter/')
+      setActive(isActive ? query.slug : false)
+    }
+  }, [isReady, pathname, query.slug])
+  
   return (
     <>
       <Grid columns={[2, 3, 4]} gap={3} sx={{ alignItems: 'center' }}>
@@ -30,14 +36,12 @@ export default ({ issues, showAbout, kind = '' }) => {
             <Card
               as="a"
               variant="nav"
-              sx={{ bg: getColor(i + 1), color: 'white' }}
-              style={{
-                boxShadow:
-                  active === i + 1
-                    ? `0 0 0 3px ${theme.colors.sheet}, 0 0 0 6px ${
-                        theme.colors[getColor(i + 1)]
-                      }`
-                    : theme.shadows.card
+              sx={{
+                bg: getColor(i + 1),
+                color: 'white',
+                boxShadow: active === issue
+                  ? theme => `0 0 0 3px ${theme.colors.sheet}, 0 0 0 6px ${theme.colors[getColor(i + 1)]}`
+                  : 'card'
               }}
             >
               {kind.length ? issue : formatTitle(issue)}
