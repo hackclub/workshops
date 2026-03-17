e679bb139631e3adfe5882aa0d08326b06c7e08c
Echo <github@3kh0.net>
2026-03-16 22:07:37 -0400

    fix conflict

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
-import React from 'react'
 import Analytics from '../components/analytics'
 import '@hackclub/theme/fonts/reg-ital-bold.css'
 import theme from '../lib/theme'
 import NProgress from '../components/nprogress'
 import { ThemeUIProvider } from 'theme-ui'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics as VercelAnalytics } from '@vercel/analytics/next'
 
 const MyApp = ({ Component, pageProps }) => (
   <ThemeUIProvider theme={theme}>
@@ -13,7 +12,7 @@ const MyApp = ({ Component, pageProps }) => (
     <Component {...pageProps} />
     <Analytics />
     <SpeedInsights />
-    <Analytics />
+    <VercelAnalytics />
   </ThemeUIProvider>
 )
 
