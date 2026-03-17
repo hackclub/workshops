2dadd6b6abb0306c8ada14cf13b6010b40fd13af
Echo <github@3kh0.net>
2026-03-16 22:06:51 -0400

    Add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,18 @@ import Analytics from '../components/analytics'
 import '@hackclub/theme/fonts/reg-ital-bold.css'
 import theme from '../lib/theme'
 import NProgress from '../components/nprogress'
-import { ThemeProvider } from 'theme-ui'
+import { ThemeUIProvider } from 'theme-ui'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/next'
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider theme={theme}>
+  <ThemeUIProvider theme={theme}>
     <NProgress color={theme.colors.primary} />
     <Component {...pageProps} />
     <Analytics />
     <SpeedInsights />
-  </ThemeProvider>
+    <Analytics />
+  </ThemeUIProvider>
 )
 
 export default MyApp
